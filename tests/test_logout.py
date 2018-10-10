a9f4254f536b37935fd034454e78ba0763456479
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2018-10-10 14:13:35 -0500

    Logout is now tested

---
+++ tests/test_logout.py
@@ -0,0 +1,13 @@
+from insta_api.insta_api import InstaAPI
+from tests.testing_config import username, password
+import time
+
+def test_logout():
+    insta = InstaAPI()
+    insta.login(username, password)
+    time.sleep(3)
+
+    insta.logout()
+
+    assert insta.last_resp.ok
+    assert not insta.is_loggedin
