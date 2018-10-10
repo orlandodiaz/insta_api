c3cd97c418caf7ef3e1a18d6d86e2e1cab33f0b4
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2018-10-10 14:10:44 -0500

    Fixed typo

--- insta_api/exceptions.py
+++ insta_api/exceptions.py
@@ -2,4 +2,4 @@ class LoginAuthentiationError(Exception):
     """Raised when the server fails to authenticate login credentials"""
 
 class InvalidHashtag(Exception):
-    """"Raised when user entered an invalid hashtag"""
+    """Raised when user entered an invalid hashtag"""
