3a1d71b8fb937e81769cd14b5e21d4bd59566f31
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2018-10-11 01:22:57 -0500

    Specific log requirement

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires = ['requests==2.19.1', 'requests-toolbelt==0.8.0', 'log3']
+    install_requires = ['requests==2.19.1', 'requests-toolbelt==0.8.0', 'log3==0.1.0']
 
 )
