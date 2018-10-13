273ea43cc7f043b4b1a5176516a62ccacba7bdd0
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2018-10-13 16:49:44 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="insta_api",
-    version="0.1.5",
+    version="0.1.6",
     author="Orlando Diaz",
     author_email="orlandodiaz.dev@gmail.com",
     description="Unofficial instagram API",
